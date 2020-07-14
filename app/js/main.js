1fd88cbd8879e1ad79b56083bffcbc8c6cd5bf94
Anvar Turakulov <anvarsam@mail.ru>
2020-07-14 19:30:39 +0500

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+
+$(function(){
+    // $('.btn').on('click',function(){
+    //     document.getElementById('input_login').value = "";
+    // });
+    
+    // $('.menu__btn').on('click',function(){
+    //   $('.header__menu').slideToggle();
+    // });
+
+});
